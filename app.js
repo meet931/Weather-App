a6eec6aa34220ff0946e59a3c1b4021dce7a95be
Mitesh Chaudhari <109075376+meet931@users.noreply.github.com>
2024-01-20 12:33:56 +0530

    Updated API

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_KEY = `3265874a2c77ae4a04bb96236a642d2f`;
+const API_KEY = `4c39ac95498a07b0d7a74d6dbf5a4b02`;
 const form = document.querySelector("form");
 const search = document.querySelector("#search");
 const weather = document.querySelector("#weather")
